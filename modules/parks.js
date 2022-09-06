d1e87d9779d02fd9ae20bac767125f55e078db66
Alan Chelko <achelko@gmail.com>
2022-09-06 14:16:31 +0200

    Changes after Petr call

--- modules/parks.js
+++ modules/parks.js
@@ -2,22 +2,27 @@
 
 const axios = require('axios');
 
-
 async function getParks(request, response, next) {
 
   const state = request.query.state;
-  const limit = 10;
   console.log('state', state);
-  const url = `https://developer.nps.gov/api/v1/parks?q=stateCode=${state}&limit=${limit}&api_key=${process.env.NPS_API_KEY}`;
+  const activities = request.query.activities;
+  
+  const limit = 50;
+  console.log('state', state);
+  const url = `https://developer.nps.gov/api/v1/parks?stateCode=${state}&limit=${limit}&api_key=${process.env.NPS_API_KEY}`;
   //https://developer.nps.gov/api/v1/parks?q=Biking&stateCode=ca&limit=3&api_key=qwJ7GoP7sWgRkS1TYC5CuMo1LokSbs6UR9Hkc3pB
-  // const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US&query=${city}&page=1&include_adult=false`;
-  // https://api.themoviedb.org/3/search/movie?api_key=079169378594480c9faa05367e9900ab&language=en-US&query=Pittsburgh&page=1&include_adult=false
-
+ 
   try {
     const parksResponse = await axios.get(url);
     console.log(parksResponse);
+    console.log(activities);
 
-    const dataToSend = parksResponse.data.data.map(object => {
+    const dataToSend = parksResponse.data.data.filter(object => {
+      return object.activities.some(activity => {
+        return activity.name === activities;
+      });
+    }).map(object => {
       return new Parks(object);
     });
 
@@ -26,15 +31,12 @@ async function getParks(request, response, next) {
   } catch (error) {
     next(error);
   }
-
 }
-
 
 class Parks {
   constructor(parksObj) {
     this.parks = parksObj.fullName;
   }
 }
-
 
 module.exports = getParks;
